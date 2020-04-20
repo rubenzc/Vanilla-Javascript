2bfd346f696393b408d42b12b2fa39d1c1ebbd21
rubenzc <rubenzc@me.com>
2020-04-20 08:25:26 +0200

    remove tweet function

--- Local Storage/assets/js/app.js
+++ Local Storage/assets/js/app.js
@@ -9,8 +9,11 @@ eventListeners();
 
 function eventListeners() {
     
-    //Submit form
+    //Submit form to add tweet
     document.querySelector('#form').addEventListener('submit', addTweet);
+
+    //Remove tweet with delegation
+    tweetList.addEventListener('click', removeTweet);
 
 }
 
@@ -23,11 +26,26 @@ function addTweet(e){
 
     //Read text area value
     const tweet = document.getElementById('tweet').value;
+    const removeButton = document.createElement('a');
+    removeButton.classList = 'remove-tweet';
+    removeButton.innerText = 'X';
 
     //Create element and add content to the list
     const li = document.createElement('li');
     li.innerText = tweet;
+    //Add remove button to tweet
+    li.appendChild(removeButton);
+    //Add tweet to the list
     tweetList.appendChild(li);
 
     console.log(tweet);
+}
+
+//Remove tweet
+
+function removeTweet(e){
+    e.preventDefault();
+    if(e.target.className === 'remove-tweet'){
+        console.log(e.target.parentElement.remove());
+    } 
 }
