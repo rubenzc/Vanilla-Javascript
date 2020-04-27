f314715c9c679ca3dbd1affa876505345ada0c06
rubenzc <rubenzc@me.com>
2020-04-27 08:31:01 +0200

    Add method & Show error message

--- Weekly Expense/js/app.js
+++ Weekly Expense/js/app.js
@@ -1,11 +1,12 @@
 //Variables
 const userBudget = prompt('Which is your weekly budget?');
+const form = document.getElementById('add-expense');
 let budgetQuantity;
+
 
 //Classes
 
 //Budget class
-
 class Budget {
     //Inside constructor because we got it from prompt
     constructor(budget) {
@@ -19,15 +20,66 @@ class Budget {
 
 }
 
+class Interface {
+    insertBudget(quantity) {
+        const budgetSpan = document.querySelector('span#total');
+        const remainingSpan = document.querySelector('span#remaining');
+
+        //Insert to HTML
+        budgetSpan.innerHTML = `${quantity}`;
+        remainingSpan.innerHTML = `${quantity}`;
+    }
+
+    printMessage(message, type) {
+        const divMessage = document.createElement('div');
+        divMessage.classList.add('text-center', 'alert');
+        if(type === 'error') {
+            divMessage.classList.add('alert-danger');
+        } else {
+            divMessage.classList.add('alert-success');
+        }
+        divMessage.appendChild(document.createTextNode(message));
+        //Insert to DOM
+        document.querySelector('.primary').insertBefore(divMessage, form);
+        //Show message 3"
+        setTimeout(function(){
+            document.querySelector('.primary .alert').remove();
+            form.reset();
+        }, 3000);
+    }
+}
+
 
 //Event listeners
 
 document.addEventListener('DOMContentLoaded', function(){
+    //Check Prompt content
     if(userBudget === null || userBudget === '') {
         window.location.reload();
     } else {
-        //Instance a new budget
+        //Instance a new Budget
         budgetQuantity = new Budget(userBudget);
-        console.log(budgetQuantity);
+
+        //Instance Interface class
+        const ui = new Interface();
+        ui.insertBudget(budgetQuantity.budget);
     }
+    
 });
+//Add button
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    //Read from expenses form
+    const expenseName = document.querySelector('#expense').value;
+    const expenseQuantity = document.querySelector('#quantity').value;
+
+    //Instance Interface
+    const ui = new Interface();
+    if(expenseName === '' || expenseQuantity === '') {
+        //Parameters(message, type)
+        ui.printMessage('There is a mistake', 'error');
+    } else {
+        //Insert into HTML
+    }
+})
