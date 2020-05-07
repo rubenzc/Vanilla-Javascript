e417b1164650bd20d820d3bd87436142e90d1e0c
rubenzc <rubenzc@me.com>
2020-05-07 18:22:11 +0200

    Added interface module

---
+++ Song  lyrics finder/js/interface.js
@@ -0,0 +1,4 @@
+export const formSearch = document.querySelector('#form-search'),
+             divSearch = document.querySelector('#search'),
+             divMessages = document.querySelector('#messages'),
+             divResult = document.querySelector('#result');
