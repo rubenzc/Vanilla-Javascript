6029fa709995bc04d658086edad2c36da2680aed
rubenzc <rubenzc@me.com>
2020-05-11 07:35:14 +0200

    Added initial structure

---
+++ Higher Order Functions/js/finder.js
@@ -0,0 +1,11 @@
+// Create years
+const years = document.createElement('option');
+const  max = new Date().getFullYear();
+let  min = max - 10;
+
+for(let i = max; i >  min; i--) {
+    let option =  document.createElement('option');
+    option.value = i;
+    option.innerText = i;
+    document.querySelector('#year').appendChild(option);
+}
