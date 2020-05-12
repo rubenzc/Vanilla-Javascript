10cab2dc6c3d0514f58976d060f499004126fdb9
rubenzc <rubenzc@me.com>
2020-05-12 07:56:30 +0200

    Added show cars function

--- Higher Order Functions/js/finder.js
+++ Higher Order Functions/js/finder.js
@@ -9,3 +9,174 @@ for(let i = max; i >  min; i--) {
     option.innerText = i;
     document.querySelector('#year').appendChild(option);
 }
+
+
+function getCars() {
+    return [
+        {
+            brand: 'BMW',
+            model: 'Serie 3',
+            year: 2012,
+            price: 30000,
+            doors: 4,
+            color: 'White',
+            transmission: 'automatic'
+        },
+        { brand: 'Audi', model: 'A4', year: 2018, price: 40000, doors: 4, color: 'Black', transmission: 'automatic' },
+        {
+            brand: 'Ford',
+            model: 'Mustang',
+            year: 2015,
+            price: 20000,
+            doors: 2,
+            color: 'White',
+            transmission: 'automatic'
+        },
+        { brand: 'Audi', model: 'A6', year: 2010, price: 35000, doors: 4, color: 'Black', transmission: 'automatic' },
+        {
+            brand: 'BMW',
+            model: 'Serie 5',
+            year: 2016,
+            price: 70000,
+            doors: 4,
+            color: 'Red',
+            transmission: 'automatic'
+        },
+        {
+            brand: 'Mercedes Benz',
+            model: 'Clase C',
+            year: 2015,
+            price: 25000,
+            doors: 4,
+            color: 'White',
+            transmission: 'automatic'
+        },
+        {
+            brand: 'Chevrolet',
+            model: 'Camaro',
+            year: 2018,
+            price: 60000,
+            doors: 2,
+            color: 'Red',
+            transmission: 'manual'
+        },
+        { brand: 'Ford', model: 'Mustang', year: 2019, price: 80000, doors: 2, color: 'Red', transmission: 'manual' },
+        {
+            brand: 'Dodge',
+            model: 'Challenger',
+            year: 2017,
+            price: 40000,
+            doors: 4,
+            color: 'White',
+            transmission: 'automatic'
+        },
+        { brand: 'Audi', model: 'A3', year: 2017, price: 55000, doors: 2, color: 'Black', transmission: 'manual' },
+        {
+            brand: 'Dodge',
+            model: 'Challenger',
+            year: 2012,
+            price: 25000,
+            doors: 2,
+            color: 'Red',
+            transmission: 'manual'
+        },
+        {
+            brand: 'Mercedes Benz',
+            model: 'Clase C',
+            year: 2018,
+            price: 45000,
+            doors: 4,
+            color: 'Blue',
+            transmission: 'automatic'
+        },
+        {
+            brand: 'BMW',
+            model: 'Serie 5',
+            year: 2019,
+            price: 90000,
+            doors: 4,
+            color: 'White',
+            transmission: 'automatic'
+        },
+        { brand: 'Ford', model: 'Mustang', year: 2017, price: 60000, doors: 2, color: 'Black', transmission: 'manual' },
+        {
+            brand: 'Dodge',
+            model: 'Challenger',
+            year: 2015,
+            price: 35000,
+            doors: 2,
+            color: 'Blue',
+            transmission: 'automatic'
+        },
+        {
+            brand: 'BMW',
+            model: 'Serie 3',
+            year: 2018,
+            price: 50000,
+            doors: 4,
+            color: 'White',
+            transmission: 'automatic'
+        },
+        {
+            brand: 'BMW',
+            model: 'Serie 5',
+            year: 2017,
+            price: 80000,
+            doors: 4,
+            color: 'Black',
+            transmission: 'automatic'
+        },
+        {
+            brand: 'Mercedes Benz',
+            model: 'Clase C',
+            year: 2018,
+            price: 40000,
+            doors: 4,
+            color: 'White',
+            transmission: 'automatic'
+        },
+        { brand: 'Audi', model: 'A4', year: 2016, price: 30000, doors: 4, color: 'Blue', transmission: 'automatic' }
+    ];
+}
+
+//Data Search
+//Similar to React state
+let dataSearch = {
+    brand : '',
+    year : '',
+    min : '',
+    max : '',
+    doors : '',
+    transmission : '',
+    color: ''
+}
+
+
+//DOM Loaded Event listeners
+const cars = getCars();
+
+document.addEventListener('DOMContentLoaded', () => {
+    showCars(cars);
+});
+
+//Form Event listeners
+const brand = document.querySelector('#brand');
+brand.addEventListener('input', e => {
+    dataSearch.brand = e.target.value;
+})
+
+
+function showCars(cars) {
+    
+    //Read Result element
+    const container = document.querySelector('#result');
+
+    cars.forEach(car => {
+        const carHTML = document.createElement('p');
+        carHTML.innerHTML = `
+            <p>${car.brand} ${car.model} - ${car.year} - ${car.doors} Doors - Transmission: ${car.transmission} - Price: $ ${car.price} - Color: ${car.color}</p>
+        `;
+        container.appendChild(carHTML);
+    })
+
+}
